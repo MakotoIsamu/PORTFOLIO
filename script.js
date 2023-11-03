9ece2f7bd4d655d911b8058622ae9038da059595
Dhruv <makotoisamu16@gmail.com>
2023-11-03 11:22:13 +0530

    some modifications

--- script.js
+++ script.js
@@ -1,50 +1,128 @@
-customCrsr();
-homepage();
+
+productPageHeadings();
 
 
-function homepage(){
+function productPageHeadings(){
+    // Select all elements with the class "elem"
+    const elemElements = document.querySelectorAll('.elem');
+
+    // Loop through each "elem" element and add mouseenter event listeners
+    elemElements.forEach((elem) => {
+        const textElement = elem.querySelector('.text');
+        elem.addEventListener('mouseenter', () => {
+            // Use GSAP to animate the text element
+            gsap.to(textElement, {
+                opacity: 0.5,
+                x: 50, // Translate 50px on the X-axis
+                duration: 0.3, // Duration of the animation
+            });
+        });
+
+        elem.addEventListener('mouseleave', () => {
+            // Reset the animation when the mouse leaves the element
+            gsap.to(textElement, {
+                opacity: 1, // Reset opacity
+                x: 0, // Reset X-axis translation
+                duration: 0.3,
+            });
+        });
+    });
+
+}
+
+gsap.to('.hehehe',{
+    transform: 'translateX(-100%)',
+    fontWeight: 300,
+    scrollTrigger: {
+        trigger: '.about',
+        scroller: 'body',
+        start: 'top 0%',
+        end: 'top -200%',
+        scrub: 3,
+        pin: true
+    }
+})
+
+function animateHeroSection() {
+    // Create a GSAP timeline for the animations
     const tl = gsap.timeline();
 
-    tl.from('nav p' , {
-        y:300,
-        duration:0.8,
-        opacity:0,
-    })
-    
-    gsap.from('h1', {
-        y:300,
-        duration:0.8,
-        opacity:0,
-    })
-    
-    tl.from('#lastName h5', {
-        y:-50,
-        duration:0.5,
-        opacity:0
-    })
-    
-    tl.from('#availability h4' ,{
-        y:-100,
-        duration:0.5,
-        opacity:0,
+    tl.from('nav span , .rightnav_img', {
+        y: -100,
+        opacity: 0,
+        duration: 0.8,
         stagger: 0.3
-    })
-    
-    tl.from('#home_footer p' , {
-        opacity:0,
-        duration:0.8
-    })
-}
+      });
+  
+    // Animation for the first name element
+    tl.from('#firstName h1', {
+      y: -50,
+      opacity: 0,
+      duration: 0.8,
+    });
+  
+    // Animation for the last name and location elements
+    tl.from('#lastName h1, #lastName h5', {
+      x: 100,
+      opacity: 0,
+      duration: 0.8,
+    });
+  
+    // Animation for the availability elements
+    tl.from('#availability h4', {
+      y: 20,
+      opacity: 0,
+      duration: 0.6,
+    });
+  
+    // Animation for the "home_footer" elements
+    tl.from('#home_footer p , .home_footer_img', {
+      y: 50,
+      opacity: 0,
+      duration: 0.6,
+      stagger: 0.2, // Add a stagger effect
+    });
+  
+    // Add a ScrollTrigger to trigger the animation when the hero section is in view
+    ScrollTrigger.create({
+      trigger: ".hero",
+      start: "top 80%", // Adjust this start point as needed
+      end: "center center", // Adjust this end point as needed
+      animation: tl,
+    });
+  }
+  
+  animateHeroSection();
 
-function customCrsr(){
-    const mainEl = document.getElementById('main');
-    const crsr = document.getElementById('cursor');
-    mainEl.addEventListener('mousemove', function(e){
-        crsr.style.left = e.clientX+'px';
-        crsr.style.top = e.clientY+'px';
-        crsr.style.opacity = 1;
-    })
-    mainEl.addEventListener('mouseleave', function(){
-        crsr.style.opacity = 0;
-    })
-}
+  function animateProjectsSection() {
+    // Create a GSAP timeline for the animations
+    const tl = gsap.timeline();
+  
+    // Define the elements to animate
+    const elements = document.querySelectorAll(".elem");
+  
+    // Animation for elements coming from the left
+    elements.forEach((elem, index) => {
+      const direction = index % 2 === 0 ? -1 : 1; // Alternate left and right
+  
+      tl.from(elem, {
+        x: 1000 * direction,
+        opacity: 0,
+        duration: 0.8,
+        delay: 0.3 * index,
+      });
+    });
+  
+    // Add a ScrollTrigger to trigger the animation
+    ScrollTrigger.create({
+      trigger: ".projects",
+      start: "top 80%", // Adjust this start point as needed
+      end: "center center", // Adjust this end point as needed
+      animation: tl,
+      toggleActions: "play none none none",
+    });
+  }
+  
+  animateProjectsSection();
+
+  
